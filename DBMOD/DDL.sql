d4c802567cf939aeb777962dab61b99178e26445
kellyp11 <57366750+kellyp11@users.noreply.github.com>
2022-04-24 20:45:34 -0400

    Update DDL.sql

--- DBMOD/DDL.sql
+++ DBMOD/DDL.sql
@@ -25,16 +25,31 @@ SELECT cast(EXTRACT(YEAR FROM StartDate) as varchar(4)) AS Year, Cost, Usage_Amo
 FROM DATE_INTERVAL NATURAL JOIN MAPS_TO NATURAL JOIN ENERGY_SOURCE_COST NATURAL JOIN ENERGY_SOURCE
 GROUP BY EXTRACT(YEAR FROM StartDate), Cost, Usage_Amount, Meter_Type;
 
+CREATE VIEW YEAR_SOURCE AS
+SELECT Year, SUM(Cost) AS Cost, SUM(Usage_Amount) AS Usage_Amount,(SUM(Usage_Amount)/cast(SUM(Cost) as float)) AS kbtuPerCost
+FROM YEAR_ENERGY_SOURCE_KBTU_COST
+GROUP BY Year, Cost;
+
 CREATE VIEW MONTH_ENERGY_SOURCE_KBTU_COST AS
 SELECT cast(EXTRACT(YEAR FROM StartDate) as varchar(4)) AS Year, cast(EXTRACT(MONTH FROM StartDate) as varchar(2)) AS Month, Cost, Usage_Amount, Usage_Amount/cast(SUM(Cost) as float) AS kbtuPerCost, Meter_Type
 FROM DATE_INTERVAL NATURAL JOIN MAPS_TO NATURAL JOIN ENERGY_SOURCE_COST NATURAL JOIN ENERGY_SOURCE
 GROUP BY EXTRACT(YEAR FROM StartDate), EXTRACT(MONTH FROM StartDate), Cost, Usage_Amount, Meter_Type;
+
+CREATE VIEW MONTH_SOURCE AS
+SELECT Year, Month, SUM(Cost) AS Cost, SUM(Usage_Amount) AS Usage_Amount, (SUM(Usage_Amount)/cast(SUM(Cost) as float)) AS kbtuPerCost
+FROM MONTH_ENERGY_SOURCE_KBTU_COST
+GROUP BY Year, Month;
 
 CREATE VIEW MINUTE_ENERGY_SOURCE_KBTU_COST AS
 SELECT StartDate, StartTimestamp, cast(Cost as float)/(30 * 24 * 4) AS cost, cast(Usage_Amount as float)/ (30*24*4) AS Usage_Amt, (Usage_Amount/(cast(Cost as float)) / (30 * 24 * 4)) AS kbtuPerCost, Meter_Type
 FROM DATE_INTERVAL NATURAL JOIN MAPS_TO NATURAL JOIN ENERGY_SOURCE_COST NATURAL JOIN ENERGY_SOURCE
 GROUP BY StartDate, StartTimestamp, Usage_Amount, Cost, Meter_Type
 ORDER BY StartDate ASC;
+
+CREATE VIEW MINUTE_SOURCE AS
+SELECT StartDate, StartTimestamp, SUM(cost) AS Cost, SUM(Usage_Amt) AS Usage_Amt, (SUM(Usage_Amt)/cast(SUM(Cost) as float)) AS kbtuPerCost
+FROM MINUTE_ENERGY_SOURCE_KBTU_COST
+GROUP BY StartDate, StartTimestamp;
 
 CREATE VIEW YEAR_METER_COST AS
 SELECT cast(EXTRACT(YEAR FROM StartDate) as varchar(4)) AS YEAR, Meter_Type, Cost
